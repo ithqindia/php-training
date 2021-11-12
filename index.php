ee971dd8007f82cc82839fc2e2d908696139d083
Amol Wankhede <amolsw.nz@gmail.com>
2021-11-12 11:48:24 +0530

    addition

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Training</title>
+</head>
+
+<body>
+    <h1>This is just a test output</h1>
+    <h2>What is 1+2? </h2>
+    <h2>The answer is <?php echo 1 + 2; ?></h2>
+</body>
+
+</html>
